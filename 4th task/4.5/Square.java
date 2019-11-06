0909751497bf7abaac3c65551cd7498e27109961
achadaev <andrew.chadaev@gmail.com>
2019-11-06 19:01:47 +0300

    4.5 task solution added

---
+++ 4th task/4.5/Square.java
@@ -0,0 +1,41 @@
+public class Square extends Rectangle {
+    public Square() {
+    }
+
+    public Square(double side) {
+        super(side, side);
+    }
+
+    public Square(String color, boolean filled, double side) {
+        super(color, filled, side, side);
+    }
+
+    public double getSide() {
+        return super.getLength();
+    }
+
+    public void setSide(double side) {
+        super.setLength(side);
+        super.setWidth(side);
+    }
+
+    @Override
+    public void setWidth(double side) {
+        super.setWidth(side);
+    }
+
+    @Override
+    public void setLength(double side) {
+        super.setLength(side);
+    }
+
+    @Override
+    public String toString() {
+        return "Square[Shape[" +
+                "color='" + super.getColor() + '\'' +
+                ", filled=" + super.isFilled() +
+                "], " +
+                "side=" + super.getWidth() +
+                ']';
+    }
+}
