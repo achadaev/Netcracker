ee48bc1650edf06cc3b5c24e2f350f683c45ad1e
achadaev <andrew.chadaev@gmail.com>
2019-11-06 18:22:57 +0300

    4.2 task solution added

---
+++ 4th task/4.2/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Point2D p2 = new Point2D(1.4f, 2.8f);
+        System.out.println(p2);
+
+        Point3D p3 = new Point3D(4.5f, 6.0f, 1.1f);
+        System.out.println(p3);
+    }
+}
