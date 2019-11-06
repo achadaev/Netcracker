8bd3bab882ff6f715226d4b734eafc7cb04a3fe0
achadaev <andrew.chadaev@gmail.com>
2019-11-06 20:15:06 +0300

    fixed error in Main class

--- 2nd task/Main.java
+++ 2nd task/Main.java
@@ -1,4 +1,11 @@
-public class Main() {
+public class Main {
 	public static void main(String[] args) {
+		Author author = new Author("Chekhov", "qwe@gmail.com", 'm');
+		Author author3 = new Author("Chekhov", "qwe@gmail.com", 'm');
+		Author author2 = new Author("Black", "black@gmail.com", 'm');
+
+		System.out.println(author.equals(author3));
+		System.out.println("author.hashCode() = " + author.hashCode());
+		System.out.println("author3.hashCode() = " + author3.hashCode());
 	}
 }
